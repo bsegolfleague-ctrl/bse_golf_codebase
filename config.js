00a1c10c06aff68d48805af9df9b45cd62879071
bsegolfleague-ctrl <bsegolfleague@gmail.com>
2025-09-05 13:35:45 -0500

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
-const API_URL = "https://script.google.com/macros/s/AKfycbyLYEBMdSZwyVZxtDJobNrmh4UFgKwLbXJiPs3wDpe9ZvfFKuJK_G2fkq7mrTtVGzj4_g/exec";
+const API_URL = "https://script.google.com/macros/s/AKfycbxTkg_UEZfQ1O_0kYvOgnyDPgSdjBvrTETbP2CjtaucqU5heJPJ1ap-cHZy4754ZDbkLw/exec";
 
 // Global compatibility alias
 const APPS_SCRIPT_URL = API_URL;
