d4274815c5deafcabdd5b3c44a064adf89e2834c
bsegolfleague-ctrl <bsegolfleague@gmail.com>
2025-09-05 12:18:07 -0500

    Update config.js

--- config.js
+++ config.js
@@ -1,1 +1,1 @@
-const APPS_SCRIPT_URL = "https://script.google.com/macros/s/AKfycbz-1pAjixcFtho7Z7Isr6iYUOuLidHk5OoghUyUWHp0Qv0lFN0CaJnuhFv057XewwfJ-w/exec";
+const APPS_SCRIPT_URL = "https://script.google.com/macros/s/AKfycby6FdYAerBeu65KgsyWRAqwzAtM773jF5rf9XzngV2uz4vN1LUzKbT7eIdxeNNe8J2MOQ/exec";
