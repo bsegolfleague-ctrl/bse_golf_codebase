cbf9386128e8d7d1acbcd67a712f9a482aac78e0
bsegolfleague-ctrl <bsegolfleague@gmail.com>
2025-09-05 10:33:01 -0500

    Update config.js

--- config.js
+++ config.js
@@ -3,5 +3,4 @@
 // 2) Paste the Web App URL below
 
 const CONFIG = {
-APPS_SCRIPT_URL: 'https://script.google.com/macros/s/AKfycbwQ2z6Bie3IMeAvKKTBPcT1nHG_I75zCeb3UCnjy4PrDQswCskth-vPCCQ3_nsX3II2cw/exec'
-};
+APPS_SCRIPT_URL: 'https://script.google.com/macros/s/AKfycbyvhU3qWTQYOsoX1yhVDM67aANTk9lJcKAJpPZ7hMvGv8VZ240OuHp34RzPrbywwIFhEg/exec'
