0a2bd6db5e9c805f7c7819b68b38112987ceba01
bsegolfleague-ctrl <bsegolfleague@gmail.com>
2025-09-05 12:25:15 -0500

    Update config.js

--- config.js
+++ config.js
@@ -1,1 +1,15 @@
-const APPS_SCRIPT_URL = "https://script.google.com/macros/s/AKfycby6FdYAerBeu65KgsyWRAqwzAtM773jF5rf9XzngV2uz4vN1LUzKbT7eIdxeNNe8J2MOQ/exec";
+const API_URL = "https://script.google.com/macros/s/AKfycbxoPhbVb2O-OV4C5huz15Ay58r20qJKYpR72_qYl99VNgVk9TbgXjGMZenjilNga1aRkQ/exec";
+
+// READ example
+fetch(API_URL)
+  .then(res => res.json())
+  .then(data => console.log(data));
+
+// WRITE example
+fetch(API_URL, {
+  method: "POST",
+  headers: { "Content-Type": "application/json" },
+  body: JSON.stringify({ Name: "New Player", Score: 75, Date: "2025-09-05" })
+})
+.then(res => res.json())
+.then(data => console.log(data));
