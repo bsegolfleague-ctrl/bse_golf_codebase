dbf4d8d4ec801723f6b6150d50240cdb015cf8ff
bsegolfleague-ctrl <bsegolfleague@gmail.com>
2025-09-04 22:46:09 -0500

    Create config.js

---
+++ config.js
@@ -0,0 +1,7 @@
+// Place this file in the root of your repo
+// 1) Deploy your Google Apps Script as a Web App
+// 2) Paste the Web App URL below
+
+const CONFIG = {
+APPS_SCRIPT_URL: 'PASTE_YOUR_WEB_APP_URL_HERE'
+};
