0434e45627726e9fe5331d9686673599d125651d
bsegolfleague-ctrl <bsegolfleague@gmail.com>
2025-09-05 12:42:24 -0500

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
-const API_URL = "https://script.google.com/macros/s/AKfycbxRYJMyUgFrouSK7iYfM45rr_HeOw9sTRWu1X4uy9igMrGJCsnleDQXUGDNyvYvRy7V_Q/exec";
+const API_URL = "https://script.google.com/macros/s/AKfycbygN4RkE8M04_oPudIZKrZmibGnrotF13W80a7KXPQkabREbaLPZTDPiGuapbOE7d8q0A/exec";
 
 fetch(API_URL)
   .then(res => res.text())  // fetch as text, not JSON directly
